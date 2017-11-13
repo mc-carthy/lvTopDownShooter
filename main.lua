4da022649009a349f60f7af87a39030c00ddc715
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-13 22:52:10 +0000

    Get basic player movement working

--- main.lua
+++ main.lua
@@ -4,15 +4,33 @@ function love.load()
   sprites.bullet = love.graphics.newImage('sprites/bullet.png')
   sprites.zombie = love.graphics.newImage('sprites/zombie.png')
   sprites.background = love.graphics.newImage('sprites/background.png')
+
+  player = {}
+  player.x = 200
+  player.y = 200
+  player.speed = 1
 end
 
 function love.update(dt)
-
+  movePlayer()
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.zombie, 100, 100)
-  love.graphics.draw(sprites.player, 300, 100)
-  love.graphics.draw(sprites.bullet, 200, 100)
+  love.graphics.draw(sprites.player, player.x, player.y)
+end
+
+function movePlayer()
+  if (love.keyboard.isDown("w")) then
+    player.y = player.y - player.speed
+  end
+  if (love.keyboard.isDown("a")) then
+    player.x = player.x - player.speed
+  end
+  if (love.keyboard.isDown("s")) then
+    player.y = player.y + player.speed
+  end
+  if (love.keyboard.isDown("d")) then
+    player.x = player.x + player.speed
+  end
 end
