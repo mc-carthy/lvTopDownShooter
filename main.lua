a843d62c016e61952ddec3971a5c7b22cd2cf06b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-14 19:42:24 +0000

    Player rotates to face mouse point

--- main.lua
+++ main.lua
@@ -17,10 +17,11 @@ end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.player, player.x, player.y)
+  love.graphics.draw(sprites.player, player.x, player.y, player_mouse_angle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
 end
 
 function movePlayer(dt)
+  -- TODO - Normalise movement vector so diagonal movement isn't faster
   if (love.keyboard.isDown("w")) then
     player.y = player.y - (player.speed * dt)
   end
@@ -33,4 +34,8 @@ function movePlayer(dt)
   if (love.keyboard.isDown("d")) then
     player.x = player.x + (player.speed * dt)
   end
+end
+
+function player_mouse_angle()
+  return math.atan2(love.mouse.getY() - player.y, love.mouse.getX() - player.x)
 end
